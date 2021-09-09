f625239cefc29fae8583cae42e7a6cf1acd31fc4
YDundie <mustacmarko@yahoo.com>
2021-09-09 15:44:15 +0200

    Async api tests

---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,21 @@
+import { render, screen, getByTestId, findByTestId } from '@testing-library/react';
+import FollowersList from '../FollowersList';
+import { BrowserRouter } from 'react-router-dom'
+
+const MockFollowersList = () => {
+    return <BrowserRouter>
+        <FollowersList></FollowersList>
+    </BrowserRouter>
+}
+
+it('should render followers ', async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId("follower-item-0")
+    expect(followerDivElement).toBeInTheDocument()
+});
+
+it('should render 5 followers ', async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findAllByTestId(/follower-item/i)
+    expect(followerDivElement.length).toBe(5)
+});
